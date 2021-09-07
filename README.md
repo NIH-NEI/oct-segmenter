b9e94e817c177cc4d8d43f6fa0b7ea28f09ac74c
Bruno Alvisio <bruno@bioteam.net>
2021-09-07 16:39:42 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Image Preprocessing
+
+This repository contains the scripts to preprocess the images shared
+by NIH-NEI.
+
+The processed images can be used as input to the model in NIH-NEI/ML-Image-Segmentation
---
+++ image-pre-processing/utils.py
@@ -0,0 +1,40 @@
+import base64
+import io
+
+import numpy as np
+import PIL.ExifTags
+import PIL.Image
+import PIL.ImageOps
+
+def img_data_to_pil(img_data):
+    f = io.BytesIO()
+    f.write(img_data)
+    img_pil = PIL.Image.open(f)
+    return img_pil
+
+
+def img_data_to_arr(img_data):
+    img_pil = img_data_to_pil(img_data)
+    img_arr = np.array(img_pil)
+    return img_arr
+
+def img_b64_to_arr(img_b64):
+    img_data = base64.b64decode(img_b64)
+    img_arr = img_data_to_arr(img_data)
+    return img_arr
+
+def img_data_to_img_b64(img_data):
+    imarray = np.array(img_data)
+    img_b64 = base64.b64encode(imarray)
+    return img_b64
+
+def img_data_to_png_data(img_data):
+    with io.BytesIO() as f:
+        f.write(img_data)
+        img = PIL.Image.open(f)
+
+        with io.BytesIO() as f:
+            img.save(f, "PNG")
+            f.seek(0)
+            return img, f.read()
+
