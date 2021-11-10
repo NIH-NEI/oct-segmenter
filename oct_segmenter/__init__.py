e9766ac832d52f31247075239f4e9c274bd6882a
Bruno Alvisio <bruno@bioteam.net>
2021-11-10 14:34:30 -0800

    Release v0.2.0

--- oct_segmenter/__init__.py
+++ oct_segmenter/__init__.py
@@ -7,4 +7,4 @@ __appname__ = "octsegmenter"
 # 1. MAJOR version when you make incompatible API changes;
 # 2. MINOR version when you add functionality in a backwards-compatible manner;
 # 3. PATCH version when you make backwards-compatible bug fixes.
-__version__ = "0.1.0"
+__version__ = "0.2.0"
