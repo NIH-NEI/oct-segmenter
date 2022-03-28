deac046a4e658ca59ac0a0bbfa38b5d98965aab8
Bruno Alvisio <bruno@bioteam.net>
2022-03-28 15:22:41 -0300

    Added scripts to preprocess masks

--- oct_segmenter/preprocessing/generic_dataset.py
+++ oct_segmenter/preprocessing/generic_dataset.py
@@ -7,6 +7,7 @@ import math
 from pathlib import Path
 
 from oct_segmenter.preprocessing.image_labeling_labelme import generate_image_label_labelme
+from oct_segmenter.preprocessing.image_labeling_mask import generate_image_label_mask
 from oct_segmenter.preprocessing.image_labeling_visual_core import generate_image_label_visual_core
 from oct_segmenter.preprocessing.image_labeling_wayne import generate_image_label_wayne
 
@@ -57,6 +58,40 @@ def process_directory_wayne(input_dir, output_dir, save_file=False):
                 image_file = Path(os.path.join(subdir, file))
                 print(f"Processing file from Wayne State University format: {image_file}")
                 img_name, img_array, seg_map, segs = generate_image_label_wayne(image_file, output_dir, save_file)
+                if img_name:
+                    img_file_names.extend([img_name])
+                    img_file_data.extend([img_array])
+                    labeled_file_data.extend([seg_map])
+                    segments_data.extend([segs])
+
+    if len(img_file_data) == 0:
+        log.info("No images were processed successfully. Exiting...")
+        exit(1)
+
+    # Images a transposed so dim 0 is image width
+    if not all(x.shape[0] == img_file_data[0].shape[0] for x in img_file_data):
+        log.error("Images contain different widths. All images should have same width. Exiting...")
+        exit(1)
+
+    if not all(x.shape[1] == img_file_data[0].shape[1] for x in img_file_data):
+        log.error("Images contain different heights. All images should have same height. Exiting...")
+        exit(1)
+
+    return img_file_names, img_file_data, segments_data, labeled_file_data
+
+
+def process_directory_mask(input_dir, output_dir, save_file=False):
+    img_file_names = []
+    img_file_data = [] # Original image (xhat)
+    labeled_file_data = [] # Segmenation map (yhat)
+    segments_data = [] # Contains the boundaries
+
+    for subdir, dirs, files in os.walk(input_dir):
+        for file in files:
+            if (file.endswith(".tiff") or file.endswith(".TIFF")) and not file.startswith("."):
+                image_file = Path(os.path.join(subdir, file))
+                print(f"Processing file from Wayne State University in mask format: {image_file}")
+                img_name, img_array, seg_map, segs = generate_image_label_mask(image_file, output_dir, save_file)
                 if img_name:
                     img_file_names.extend([img_name])
                     img_file_data.extend([img_array])
@@ -168,6 +203,9 @@ def generate_generic_dataset(
     elif input_format == "labelme":
         img_file_names, img_file_data, segments_data, labeled_file_data = \
             process_directory_labelme(input_dir, str(file_name.parent), save_file=False)
+    elif input_format == "mask":
+        img_file_names, img_file_data, segments_data, labeled_file_data = \
+            process_directory_mask(input_dir, str(file_name.parent), save_file=False)
     elif input_format == "none":
         img_file_names, img_file_data, segments_data, labeled_file_data = \
             process_directory(input_dir, str(file_name.parent), save_file=False)
