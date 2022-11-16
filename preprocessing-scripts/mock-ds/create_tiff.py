d3d9646dc20ad33bf9e9738fb66aa349ce35b024
Bruno Alvisio <bruno@bioteam.net>
2022-11-16 12:53:47 -0600

    Add script to create TIFF files

---
+++ preprocessing-scripts/mock-ds/create_tiff.py
@@ -0,0 +1,13 @@
+"""
+Helper script to create TIFF files.
+
+Example:
+    python preprocessing-scripts/mock-ds/create_tiff.py
+"""
+
+import numpy as np
+from PIL import Image
+
+data = np.random.randint(0, 255, (2,2)).astype(np.uint8)
+im = Image.fromarray(data)
+im.save('test.tiff')
