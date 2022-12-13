19b3114922e8cc7eeb557ecb4cd9f00fb285501d
Bruno Alvisio <bruno@bioteam.net>
2022-12-13 09:25:02 +0000

    Add multiple thresholds to find the IPL and smoothing data function

--- preprocessing-scripts/pipeline/pipeline.m
+++ preprocessing-scripts/pipeline/pipeline.m
@@ -1,6 +1,6 @@
 function pipeline( ... 
     input_dir, ce_output_dir, trimmed_ce_output_dir, output_dir, ...
-    annot_output_dir, resized_width, resized_height ...
+    annot_output_dir, resized_width, resized_height, multiple ...
 )
     %
     % This function takes a input directory containing OCT images in .tiff
@@ -34,15 +34,28 @@ function pipeline( ...
     % estimates of the ILM (yellow) and the fitted polynomial (green)
     %
     % [int]
-    % resized_width: Width to which the images should be resized to.
+    % resized_width: Width to which the images should be resized to. Use -1
+    % to use the maximum height found after cropping the image.
     %
     % [int]
     % resized_height: Height to which the images should be resized to.
     %
+    % [int]
+    % multiple: The 'resize_height' or 'max_height' will be increased
+    % enough to make the final height of the images a multiple of the
+    % parameter value.
+    %
     % Sample usage:
     %
     % >> pipeline("test-in", "ce-test-out", "trimmed-ce-test-out", ...
-    %        "test-out", "test-annot", 384, 480)
+    %        "test-out", "test-annot", 384, 480, 16)
+    %
+    % For using the "max height" found in the set of input images:
+    %
+    % pipeline("test-in", "ce-test-out", "trimmed-ce-test-out", ...
+    %        "test-out", "test-annot", 384, -1, 16)
+    %
+    % $Author: Bruno Alvisio (bruno@bioteam.net)
     %
     addpath("oct-image-contrast-enhancement/");
     addpath("trimming/");
@@ -51,26 +64,41 @@ function pipeline( ...
     enhance_images_contrast(input_dir, ce_output_dir);
 
     % Cropping to remove background
-    crop_oct_images(ce_output_dir, trimmed_ce_output_dir, ...
+    max_height = crop_oct_images(ce_output_dir, trimmed_ce_output_dir, ...
         annot_output_dir, input_dir ...
     );
 
+    disp("The max height of the images after cropping is " + max_height);
+
     % Resizing
+    if resized_height == -1
+        new_height = max_height;
+        disp("Max height found is: " + new_height);
+    else
+        new_height = resized_height;
+    end
+    % Make height multiple of 'multiple' param.
+    if rem(new_height, multiple) >  0
+        new_height = new_height + (multiple - rem(new_height, multiple));
+    end
+    disp("Resizing to height: " + new_height);
+
     for trimmed_input_image = dir(trimmed_ce_output_dir + "/*.tiff")'
         input_image_name = trimmed_input_image.folder + "/" + ...
             trimmed_input_image.name;
-        disp("Processing Image resize: " + input_image_name)
+        %disp("Processing Image resize: " + input_image_name)
         img = imread(input_image_name);
-        resized_img = imresize(img, [resized_height, resized_width]);
+        resized_img = imresize(img, [new_height, resized_width]);
         output_image_name = output_dir + "/" + trimmed_input_image.name;
         imwrite(resized_img, output_image_name);
-            
+
         [~, mask_file_name, ~] = fileparts(trimmed_input_image.name);
         mask_image_name = trimmed_input_image.folder + "/" + ...
         mask_file_name + ".csv";
-        disp("Processing Mask resize: " + mask_image_name);
+        %disp("Processing Mask resize: " + mask_image_name);
         mask = readmatrix(mask_image_name);
-        resized_mask = imresize(mask, [resized_height, resized_width], ...
+
+        resized_mask = imresize(mask, [new_height, resized_width], ...
             "nearest" ...
         );
         output_mask_name = output_dir + "/" + mask_file_name + ".csv";
--- preprocessing-scripts/pipeline/trimming/crop_oct_images.m
+++ preprocessing-scripts/pipeline/trimming/crop_oct_images.m
@@ -1,5 +1,5 @@
-function crop_oct_images(input_dir, output_dir, annot_output_dir, ...
-    csv_input_dir ...
+function max_height = crop_oct_images(input_dir, output_dir, ...
+    annot_output_dir, csv_input_dir ...
 )
     %
     % Using a 'threshold' value, it estimates the location of the ILM
@@ -32,70 +32,109 @@ function crop_oct_images(input_dir, output_dir, annot_output_dir, ...
     % annot_output_dir: Output path to save the input images with the
     % estimates of the ILM (yellow) and the fitted polynomial (green)
     %
-    % [boolean]
-    % crop_csv: Apply the same cropping to the CSV corresponding to the
-    % image
+    % [string]
+    % csv_input_dir: Input path to the CSV masks
     %
-    threshold = 50; % Is computed manually by analysing the intensity of the RPE layer
+    % $Author: Bruno Alvisio (bruno@bioteam.net)
+    %
+    thresholds = [40, 30, 50];
+    curvature_threshold = 40;
     crop_top = 30;
     crop_bottom = 200;
+    max_height = -Inf;
+    top_black_band = 70; % To prevent from noise to be confused as the IPL
 
+    disp("Cropping TIFFs and CSV masks");
     for input_image = dir(input_dir + "/*.tiff")'
         input_image_name = input_image.folder + "/" + input_image.name;
-        disp("Processing crop TIFF: " + input_image_name)
         img = imread(input_image_name);
         ilm = NaN(1, size(img, 2));
 
-        for j = 1:size(img, 2)
-            ilm(j) = find(img(:,j) >= threshold, 1, 'first');
-        end
-        % Fit polynomial
-        x = 1:length(ilm);
-        p = polyfit(x, ilm, 2);
-        ilm_fit = round(polyval(p, x));
+        found_ipl = false;
+        for threshold = thresholds
+            %disp("Finding IPL with threshold " + threshold + ...
+            %    " of TIFF: " + input_image_name ...
+            %)
+            for j = 1:size(img, 2)
+                ilm(j) = find(img(top_black_band:end,j) >= threshold, ...
+                    1, 'first') + top_black_band;
+            end
 
-        img_height = size(img, 1);
-        new_top = max(1, max(ilm_fit) - crop_top);
-        new_bottom = min(img_height, max(ilm_fit) + crop_bottom);
-        if new_top == 1
-            disp("WARNING: Top margin reached top of original image")
-        end
+            img_width = length(ilm);
+            x = 1:img_width;
+            % Remove hgh-frequency noise (i.e. jumps in data)
+            smooth = smoothdata(ilm, "movmedian", 200);
+            % Fit polynomial
+            p = polyfit(x, smooth, 2);
+            ilm_fit = round(polyval(p, x));
 
-        if new_bottom == img_height
-            disp("WARNING: Bottom margin reached bottom of original image")
-        end
+            % Get a measure of the difference between the polynomial at the
+            % center and sides of the image. If the difference is too large
+            % try using another threshold for finding the IPL.
+            if abs(ilm_fit(fix(img_width/2)) - ilm_fit(1)) > curvature_threshold ...
+                || abs(ilm_fit(fix(img_width/2)) - ilm_fit(img_width)) > curvature_threshold
+                disp("WARNING: The polynomial fit has a curvature " + ...
+                    "greater than expected for image: " + ...
+                    input_image_name + ". Trying with another " + ...
+                    "threshold." ...
+                );
+                continue;
+             end
 
-        cropped_img = img(new_top : new_bottom, :);
-        output_image_name = output_dir + "/" + input_image.name;
-        imwrite(cropped_img, output_image_name);
+            img_height = size(img, 1);
+            new_top = max(1, min(ilm_fit) - crop_top);
+            new_bottom = min(img_height, max(ilm_fit) + crop_bottom);
+            found_ipl = true;
+            new_height = new_bottom - new_top;
+            if new_height > max_height
+                max_height = new_height;
+            end
 
-        % Interleave x and ILM
-        x_t = x(:).';
-        ilm_t = ilm(:).';
-        x_ilm_concat = [x_t; ilm_t]; % concatenate them vertically
-        x_ilm_interleaved = x_ilm_concat(:);
+            cropped_img = img(new_top : new_bottom, :);
+            output_image_name = output_dir + "/" + input_image.name;
+            imwrite(cropped_img, output_image_name);
 
-        % Interleave x and fitted polynomial
-        ilm_fit_t = ilm_fit(:).';
-        x_ilm_fit_concat = [x_t; ilm_fit_t];
-        x_ilm_fit_interleaved = x_ilm_fit_concat(:);
+            % Interleave x and ILM
+            x_t = x(:).';
+            ilm_t = ilm(:).';
+            x_ilm_concat = [x_t; ilm_t]; % concatenate them vertically
+            x_ilm_interleaved = x_ilm_concat(:);
 
-        img = insertShape(img, "line", ...
-            [x_ilm_interleaved'; x_ilm_fit_interleaved'], ...
-            "Color", ["yellow", "green"] ...
-        );
+            % Interleave x and fitted polynomial
+            ilm_fit_t = ilm_fit(:).';
+            x_ilm_fit_concat = [x_t; ilm_fit_t];
+            x_ilm_fit_interleaved = x_ilm_fit_concat(:);
 
-        imwrite(img, annot_output_dir + "/" + input_image.name);
+            % Interleave smooth
+            smooth_t = smooth(:).';
+            smooth_concat = [x_t; smooth_t];
+            smooth_interleaved = smooth_concat(:);
 
-        if exist(csv_input_dir)
-            [~, file_name, ~] = fileparts(input_image.name);
-            input_csv_file_name = csv_input_dir + "/" + ...
-                 file_name + ".csv";
-            disp("Processing crop CSV: " + input_csv_file_name);
-            mask = readmatrix(input_csv_file_name);
-            mask = mask(new_top : new_bottom, :);
-            output_csv_file_name = output_dir + "/" + file_name + ".csv";
-            writematrix(mask, output_csv_file_name);
+            img = insertShape(img, "line", ...
+                [x_ilm_interleaved'; x_ilm_fit_interleaved'; ...
+                smooth_interleaved'], ...
+                "Color", ["yellow", "green", "red"] ...
+            );
+
+            imwrite(img, annot_output_dir + "/" + input_image.name);
+
+            if exist(csv_input_dir)
+                [~, file_name, ~] = fileparts(input_image.name);
+                input_csv_file_name = csv_input_dir + "/" + ...
+                    file_name + ".csv";
+                %disp("Cropping CSV: " + input_csv_file_name);
+                mask = readmatrix(input_csv_file_name);
+                mask = mask(new_top : new_bottom, :);
+                output_csv_file_name = output_dir + "/" + file_name + ...
+                    ".csv";
+                writematrix(mask, output_csv_file_name);
+            end
+            break;
+        end % end threshold
+        if ~found_ipl
+            disp("WARNING: Couldn't find IPL layer after trying all " + ...
+                "thresholds for image: " + input_image_name ...
+            );
         end
     end
 end
